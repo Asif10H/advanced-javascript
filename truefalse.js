c5a1f18ea788b7d123c09de8ed7238011cdaa4f4
Asif10H <habibullahasif71@gmail.com>
2021-02-03 16:09:12 +0600

    true vs false

---
+++ truefalse.js
@@ -0,0 +1,18 @@
+//Falsy: 
+//false
+// 0
+//""
+// undefine
+//null
+//NaN
+//Truthy:
+//"0", " ", [], {}
+
+let name = 0;
+console.log(name);
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
